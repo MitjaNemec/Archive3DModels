a18b87fa05854842f2abb45f656e895d47a2e458
Mitja Nemec <mitja.nemec@fe.uni-lj.si>
2022-01-07 17:00:06 +0100

    remove metadata.json

--- make_a_package.sh
+++ make_a_package.sh
@@ -6,23 +6,24 @@ inkscape --without-gui archive_3d_models_light.svg -w 24 -h 24 -o archive_3d_mod
 inkscape --without-gui archive_3d_models_light.svg -w 64 -h 64 -o archive_3d_models.png
 
 # grab version and parse it into metadata.json
+cp metadata_source.json metadata.json
 version=`cat version.txt`
 sed -i -e "s/VERSION/$version/g" metadata.json
 
 # prepare the package
 mkdir plugins
-cp config.ini plugin
-cp archive_3d_models_dark.png plugin
-cp archive_3d_models_light.png plugin
-cp __init__.py plugin
-cp action_archive_3d_models.py plugin
-cp archive_3d_models_end_GUI.py plugin
-cp archive_3d_models_main_GUI.py plugin
-cp archive_3d_models_settings_GUI.py plugin
-cp archive_3d_models.py plugin
-cp version.txt plugin
+cp config.ini plugins
+cp archive_3d_models_dark.png plugins
+cp archive_3d_models_light.png plugins
+cp __init__.py plugins
+cp action_archive_3d_models.py plugins
+cp archive_3d_models_end_GUI.py plugins
+cp archive_3d_models_main_GUI.py plugins
+cp archive_3d_models_settings_GUI.py plugins
+cp archive_3d_models.py plugins
+cp version.txt plugins
 mkdir resources
-cp archive_3d_models.jpg resources/icon.png
+cp archive_3d_models.png resources/icon.png
 
 zip -r Archive3DModels-$version-pcm.zip plugins resources metadata.json
 
