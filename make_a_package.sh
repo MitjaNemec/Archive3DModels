a4c787fe580ed15a87b24d6e465e020a12fbf3c6
Mitja Nemec <mitja.nemec@fe.uni-lj.si>
2022-01-07 19:09:46 +0100

    debugging the release process

--- make_a_package.sh
+++ make_a_package.sh
@@ -11,9 +11,15 @@ inkscape archive_3d_models_light.svg -w 64 -h 64 -o archive_3d_models.png
 ~/WxFormBuilder/bin/wxformbuilder -g Archive3DModels_settings_GUI.fbp
 
 # grab version and parse it into metadata.json
-cp metadata_source.json metadata.json
+cp metadata_source.json metadata_package.json
 version=`cat version.txt`
-sed -i -e "s/VERSION/$version/g" metadata.json
+sed -i -e "s/VERSION/$version/g" metadata_package.json
+
+# cut the download, sha and size fields
+sed -i '/download_url/d' metadata_package.json
+sed -i '/download_size/d' metadata_package.json
+sed -i '/install_size/d' metadata_package.json
+sed -i '/download_sha256/d' metadata_package.json
 
 # prepare the package
 mkdir plugins
@@ -29,9 +35,22 @@ cp archive_3d_models.py plugins
 cp version.txt plugins
 mkdir resources
 cp archive_3d_models.png resources/icon.png
+cp metadata_package.json metadata.json
 
 zip -r Archive3DModels-$version-pcm.zip plugins resources metadata.json
 
 # clean up
 rm -r resources
 rm -r plugins
+rm metadata.json
+
+# get the sha, size and fill them in the metadata
+cp metadata_source.json metadata.json
+version=`cat version.txt`
+sed -i -e "s/VERSION/$version/g" metadata.json
+zipsha=`sha256sum Archive3DModels-$version-pcm.zip | xargs | cut -d' ' -f1`
+sed -i -e "s/SHA256/$zipsha/g" metadata.json
+unzipsize=`unzip -l Archive3DModels-$version-pcm.zip | tail -1 | xargs | cut -d' ' -f1`
+sed -i -e "s/INSTALL_SIZE/$unzipsize/g" metadata.json
+dlsize=`ls -al Archive3DModels-$version-pcm.zip | tail -1 | xargs | cut -d' ' -f5`
+sed -i -e "s/DOWNLOAD_SIZE/$dlsize/g" metadata.json
